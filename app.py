37a38c3820bc4ecaf5e0f570439c08fb67edcfa9
leehuiseok4344 <lhuiseok@gmail.com>
2025-07-29 13:36:08 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,41 @@
 import streamlit as st
 import streamlit.components.v1 as htmlviewer
+st.set_page_config(layout='wide', page_title='DNA sequencying!!!')
 
-# Title Msg#1
-st.title('This is Sky Webapp!!')
+# title msg#1
+st.title("this is sky webapp!!")
 
 with open('./index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
+    html = f.read() 
     f.close()
 
-# html = '''
-# <html>
-#     <head>
-#         <title> this is my html </title>
-#     </head>
+#html = '''
+#<html>
+#    <head>
+#        <title> this is my html </title>
+#    </head>
 
-#     <body>
-#         <h1>Topic</h1>
-#         <h2>SubTopic</h2>
-#     </body>
-# </html>
-# '''
+#    <body>
+#        <h1>topic</h1>
+#        <h2>SubTopic</h2>
+#</body>
+#</html>
+#'''
 
-# Box#1(4), Box#2(1)
+# box#1(4),box#2(1)
 col1, col2 = st.columns((4,1))
 with col1:
-    with st.expander('Content #1...'):
+    with st.expander('content #1...'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
-        st.info('Content..')
+        st.info('content..')
         st.video(url)
 
-    with st.expander('Content #2...'):
-        #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, height=600)
+with st.expander('image content...'):
+        imgfile = './images/gpt.jpg'
+        st.image(imgfile)
 
-    with st.expander('Content #3...'):
-        #st.write(html, unsafe_allow_html=True)
+with st.expander('content #2...'):
+        st.write(html,unsafe_allow_html=True)
         htmlviewer.html(html, height=600)
 
 with col2:
@@ -42,4 +43,4 @@ with col2:
         st.info('Tips..')
 
 st.markdown('<hr>', unsafe_allow_html=True)
-st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True) 
+st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True)
